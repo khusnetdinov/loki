75f1546ab9ef2c36d63afd4e281c2bd4f06f6e27
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-12-15 12:39:46 +0300

    cmd tests

--- test/loki/cmd_test.exs
+++ test/loki/cmd_test.exs
@@ -1,17 +1,45 @@
 defmodule Loki.CmdTest do
   use ExUnit.Case, async: true
 
-  # import Loki.Cmd
+  import Loki.Cmd
+  import ExUnit.CaptureIO
 
-  @tag :skip
-  test "should execute command" do
-  end
+  describe "Cmd" do
+    test "#execute command in shell" do
+      assert capture_io(fn ->
+        execute "pwd"
+      end) == "\e[32m *   execute \e[0mpwd\e[0m\n"
+    end
 
-  @tag :skip
-  test "should execute in other path" do
-  end
+    @tag :pending
+    test "#execute return result => " do
+      assert execute("pwd") == System.cmd("pwd", [])
+    end
 
-  @tag :skip
-  test "should format shell output" do
+
+    test "#execute_in_path command in shell" do
+      assert capture_io(fn ->
+        execute_in_path("pwd", "..")
+      end) == "\e[32m *   execute \e[0mpwd in path ..\e[0m\n"
+    end
+
+    @tag :pending
+    test "#execute_in_path return result => " do
+      assert execute_in_path("pwd", "..") == System.cmd("pwd", [], cd: "..")
+    end
+
+
+    test "#format_output command in shell" do
+      {test_user, _} = System.cmd("whoami", [])
+      assert capture_io(fn ->
+        format_output(System.cmd("whoami", []))
+      end) == "\n\e[33m#{test_user}\e[0m\n"
+    end
+
+    @tag :pending
+    test "#format_outout return result => " do
+      assert format_output(System.cmd("whoami", [])) == :ok
+    end
   end
 end
+
--- test/loki/shell_test.exs
+++ test/loki/shell_test.exs
@@ -4,92 +4,96 @@ defmodule Loki.ShellTest do
   import Loki.Shell
   import ExUnit.CaptureIO
 
-  test "asking user input" do
-    assert capture_io("answer", fn ->
-      ask("Test question?")
-      send self(), "answer"
-    end) == "Test question?"
 
-    assert_received "answer"
-  end
+  describe "Shell" do
+    test "asking user input" do
+      assert capture_io("answer", fn ->
+        ask("Test question?")
+        send self(), "answer"
+      end) == "Test question?"
 
-  test "asking yes?" do
-    assert capture_io("yes", fn ->
-      yes?("Test question?")
-      send self(), "yes"
-    end) == "Test question?"
-  end
+      assert_received "answer"
+    end
 
-  test "asking no?" do
-    assert capture_io("no", fn ->
-      yes?("Test question?")
-      send self(), "no"
-    end) == "Test question?"
-  end
+    test "asking yes?" do
+      assert capture_io("yes", fn ->
+        yes?("Test question?")
+        send self(), "yes"
+      end) == "Test question?"
+    end
 
-  test "saying to shell" do
-    assert capture_io(fn ->
-      say "Saying to shell"
-    end) == "Saying to shell" <> "\n"
-  end
+    test "asking no?" do
+      assert capture_io("no", fn ->
+        yes?("Test question?")
+        send self(), "no"
+      end) == "Test question?"
+    end
 
-  test "saying create" do
-    assert capture_io(fn ->
-      say_create "file"
-    end) == "\e[32m *  creating \e[0mfile\e[0m\n"
-  end
+    test "saying to shell" do
+      assert capture_io(fn ->
+        say "Saying to shell"
+      end) == "Saying to shell" <> "\n"
+    end
 
-  test "saying force" do
-    assert capture_io(fn ->
-      say_force "file"
-    end) == "\e[33m *     force \e[0mfile\e[0m\n"
-  end
+    test "saying create" do
+      assert capture_io(fn ->
+        say_create "file"
+      end) == "\e[32m *  creating \e[0mfile\e[0m\n"
+    end
 
-  test "saying if identic" do
-    assert capture_io(fn ->
-      say_identical "file"
-    end) == "\e[34m\e[1m * identical \e[0mfile\e[0m\n"
-  end
+    test "saying force" do
+      assert capture_io(fn ->
+        say_force "file"
+      end) == "\e[33m *     force \e[0mfile\e[0m\n"
+    end
 
-  test "saying about skip" do
-    assert capture_io(fn ->
-      say_skip "file"
-    end) == "\e[33m *      skip \e[0mfile\e[0m\n"
-  end
+    test "saying if identic" do
+      assert capture_io(fn ->
+        say_identical "file"
+      end) == "\e[34m\e[1m * identical \e[0mfile\e[0m\n"
+    end
 
-  test "saying error" do
-    assert capture_io(fn ->
-      say_error "file"
-    end) == "\e[31m *     error \e[0mfile\e[0m\n"
-  end
+    test "saying about skip" do
+      assert capture_io(fn ->
+        say_skip "file"
+      end) == "\e[33m *      skip \e[0mfile\e[0m\n"
+    end
 
-  test "saying conflict" do
-    assert capture_io(fn ->
-      say_conflict "file"
-    end) == "\e[33m *  conflict \e[0mfile\e[0m\n"
-  end
+    test "saying error" do
+      assert capture_io(fn ->
+        say_error "file"
+      end) == "\e[31m *     error \e[0mfile\e[0m\n"
+    end
 
-  test "saying exists" do
-    assert capture_io(fn ->
-      say_exists "file"
-    end) == "\e[34m\e[1m *    exists \e[0mfile\e[0m\n"
-  end
+    test "saying conflict" do
+      assert capture_io(fn ->
+        say_conflict "file"
+      end) == "\e[33m *  conflict \e[0mfile\e[0m\n"
+    end
 
-  test "saying rename" do
-    assert capture_io(fn ->
-       say_rename "file", "new_file"
-    end) == "\e[32m *    rename \e[0mfile\e[32m to \e[0mnew_file\e[0m\n"
-  end
+    test "saying exists" do
+      assert capture_io(fn ->
+        say_exists "file"
+      end) == "\e[34m\e[1m *    exists \e[0mfile\e[0m\n"
+    end
 
-  test "saying copy" do
-    assert capture_io(fn ->
-      say_copy "file", "new_file"
-    end) == "\e[32m *      copy \e[0mfile\e[32m to \e[0mnew_file\e[0m\n"
-  end
+    test "saying rename" do
+      assert capture_io(fn ->
+        say_rename "file", "new_file"
+      end) == "\e[32m *    rename \e[0mfile\e[32m to \e[0mnew_file\e[0m\n"
+    end
 
-  test "saying remove" do
-    assert capture_io(fn ->
-      say_remove "file"
-    end) == "\e[32m *    remove \e[0mfile\e[0m\n"
+    test "saying copy" do
+      assert capture_io(fn ->
+        say_copy "file", "new_file"
+      end) == "\e[32m *      copy \e[0mfile\e[32m to \e[0mnew_file\e[0m\n"
+    end
+
+    test "saying remove" do
+      assert capture_io(fn ->
+        say_remove "file"
+      end) == "\e[32m *    remove \e[0mfile\e[0m\n"
+    end
   end
 end
+
