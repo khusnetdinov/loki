069e744a5cf9ba601fb554bb8997063ebfd5efc1
Marat Khusnetdinov <marat@khusnetdinov.ru>
2016-12-16 19:09:01 +0300

    use loki

--- lib/loki.ex
+++ lib/loki.ex
@@ -4,8 +4,14 @@ defmodule Loki do
 
   @doc """
   """
+
   def __using__() do
     quote do
+      import Loki.Shell
+      import Loki.Cmd
+      import Loki.Directory
+      import Loki.File
+      import Loki.FileManipulation
     end
   end
 end
